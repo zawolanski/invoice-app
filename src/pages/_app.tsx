ddc8d5d186d2a8007c99c134f708f943c2882028
Sebastian <sebastian.zawolanski@gmail.com>
2022-10-17 20:26:17 +0200

    feat: add the next-themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,11 +8,14 @@ import type { AppType } from 'next/app';
 import type { AppRouter } from '../server/router';
 import type { Session } from 'next-auth';
 import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
 
 const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 };
